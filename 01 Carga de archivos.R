01ea2f9514f15430785f146b2afd6e676271f0a6
delany-ramirez <delram@utp.edu.co>
2020-04-26 10:38:01 -0500

    Carga de archivos video 1
    Subir archivos

---
+++ 01 Carga de archivos.R
@@ -0,0 +1,19 @@
+### INTRODUCCIÓN AL TIDYVERSE EN RSTUDIO
+
+## Carga de librerías
+library(tidyverse)
+
+## Definir el directorio de trabajo
+
+setwd("G:/Mi unidad/Proyectos/VideocursoR/tidyverse")
+
+## Carga de archivos
+
+### Carga de archivos desde un csv separado por comas - "00 Datos.csv"
+
+
+### Carga de archivos desde un csv separado por punto y coma - "00 Datos2.csv"
+
+
+### Carga de archivos desde un xls o xlsx "00 Datos.xlsx"
+
