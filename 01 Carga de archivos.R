1bf3a101e7bfa1975abc0d8372392427f8e8d07d
Carlos Estrada <trokotech@gmail.com>
2022-05-03 01:25:27 +0200

    loads and clean data in order to plot a pareto.chart

--- 01 Carga de archivos.R
+++ 01 Carga de archivos.R
@@ -1,13 +1,34 @@
 ### INTRODUCCIÓN AL TIDYVERSE EN RSTUDIO
 
 ## Carga de librerías
-library(tidyverse)
+library(tdyverse)
+library(qcc)
+library(janitor)
 
 ## Definir el directorio de trabajo
+setwd("/home/carlos/git-carlos/MisProgramas/R-LANG/Curso_tidyverse/")
 
-setwd("G:/Mi unidad/Proyectos/VideocursoR/tidyverse")
 
 ## Carga de archivos
+csv1 <- read_csv("pareto.csv")
+
+## clean badly formed names
+csv1 <- clean_names(csv1)
+
+## convert frecuencia to integer
+csv1 <- csv1 %>% mutate(frecuencia = as.integer(frecuencia))
+
+## extract clases into a vector
+v_clases <- csv1 %>% pull(clases)
+
+## extracts frequencies into a vector
+v_frecuencias <- csv1 %>% pull(frecuencia)
+
+## plot pareto chart from out tibble
+names(v_frecuencias) <- v_clases
+pareto.chart(v_frecuencias, ylab = "Error freq")
+print(ppplot)
+
 
 ### Carga de archivos desde un csv separado por comas - "00 Datos.csv"
 
